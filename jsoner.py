cb258ba185006cb8ea1a846c4e157de21edb256e
David <kot.shubin@mail.ru>
2022-08-15 14:25:10 +0300

    Редактирование файла с датами

--- jsoner.py
+++ jsoner.py
@@ -1,4 +1,14 @@
 import json
+
 with open('dates.txt', encoding='utf-8') as file:
-  info = file.readlines()
-print(*info)
+    info = file.readlines()
+jsn = []
+for k in info:
+    i = k.strip()
+    if i[0] != '#':
+        # print(i.split(' – '))
+        jsn.append((i.split(' – ')[0], ' - '.join(i.split(' – ')[1:])))
+    else:
+        jsn.append(i)
+# print(*info)
+print(*jsn, sep='\n')
