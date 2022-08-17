4ff7113486fa496d0ca7564bbd86e1dea32c9a44
David <kot.shubin@mail.ru>
2022-08-17 16:58:29 +0300

    Редактирование dates.txt, создание json файла с датами и налаживание механизма его создания. Создание придатка механизма поиска по датам

--- jsoner.py
+++ jsoner.py
@@ -2,13 +2,20 @@ import json
 
 with open('dates.txt', encoding='utf-8') as file:
     info = file.readlines()
-jsn = []
+jsn = {}
+# lol = 0
 for k in info:
     i = k.strip()
     if i[0] != '#':
-        # print(i.split(' – '))
-        jsn.append((i.split(' – ')[0], ' - '.join(i.split(' – ')[1:])))
-    else:
-        jsn.append(i)
-# print(*info)
-print(*jsn, sep='\n')
+        # lol += 1
+        # print(i.split(' – ')[1][-1], lol)
+        jsn[i.split(' – ')[0]] = i.split(' – ')[1]
+    # else:
+        # jsn.append(i)
+# print(*txtDates)
+print(jsn)
+'''with open("dates.json", "w") as file:
+    json.dump(jsn, file)'''
+'''with open("dates.json", "r") as read_file:
+    JsDates = json.load(read_file)
+print(JsDates)'''
--- main.py
+++ main.py
@@ -3,45 +3,55 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import sqlite3
 import datetime
+import json
 
+# Создание бота
 TOKEN = '5165988091:AAGZ1qrF6r7f8cB_crlOzX0dpHltfViCyA8'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+# Создание соединений с файлами, вероятно перемещение их в отдельный файл
 conn = sqlite3.connect('users.db')
+cur = conn.cursor()
+with open('dates.txt', 'r', encoding='utf-8') as file:
+    txtDates = file.readlines()
+with open("dates.json", "r") as read_file:
+    JsDates = json.load(read_file)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start'])  # Функция приветствия и регистрации
 async def start(msg: types.Message):
     await msg.reply(
         "Привет, меня зовут HistoryBot, но для тебя Хисти. Готов тебя проконсультировать по некоторым историческим вопросам!")
-    cur = conn.cursor()
-    if not cur.execute(
+    if not cur.execute(  # Регистрация юзера, если он еще не зарегистрирован
             f'''SELECT * FROM users WHERE id = {msg.from_user.id}''').fetchall():
         sql = '''INSERT INTO users(id, username, name, regs) VALUES(?, ?, ?, ?)'''
         data_tuple = (
-        msg.from_user.id, msg.from_user.username, msg.from_user.first_name, datetime.datetime.now().strftime("%Y-%m-%d"))
+            msg.from_user.id, msg.from_user.username, msg.from_user.first_name,
+            datetime.datetime.now().strftime("%Y-%m-%d"))
         cur.execute(sql, data_tuple)
         conn.commit()
 
 
-@dp.message_handler(commands=['help'])
+@dp.message_handler(commands=['help'])  # Команда помощь ¯\_(ツ)_/¯
 async def help(msg: types.Message):
     await msg.reply("Пока что команда в разработке")
 
 
-# @dp.message_handler(commands=['search']):
-# async def search(message: types.Message):
 @dp.message_handler(commands=['view_dates'])
+async def view(msg: types.Message):
+    '''with open('dates.txt', 'r', encoding='utf-8') as file:
+        txtDates = file.readlines()'''
+    for x in range(0, len('\n'.join(txtDates)), 4096):
+        await bot.send_message(msg.chat.id, '\n'.join(txtDates)[x:x + 4096])
+
+
+@dp.message_handler(commands=['browse'])  # Первый прототип поиска по датам
 async def search(msg: types.Message):
-    with open('dates.txt', 'r', encoding='utf-8') as file:
-        info = file.readlines()
-    for x in range(0, len('\n'.join(info)), 4096):
-        await bot.send_message(msg.chat.id, '\n'.join(info)[x:x + 4096])
+    '''with open("dates.json", "r") as read_file:
+        JsDates = json.load(read_file)'''
+    argument = msg.get_args()  # Получение даты
+    # TODO Создать механизм поиска нужной даты!
 
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-
-
-
-
