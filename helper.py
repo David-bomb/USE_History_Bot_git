e375bedc38c07c9c89e148e71d3db5d2c70c300f
David <kot.shubin@mail.ru>
2022-09-01 23:58:58 +0300

    Добавление поиска по датам

---
+++ helper.py
@@ -0,0 +1,69 @@
+import sqlite3
+import json
+
+
+def datesDB_rewrite():  # Переписывание БД с датами
+    conn = sqlite3.connect('dates.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS dates( 
+       date TEXT,
+       event TEXT
+       )
+    """)
+    with open("dates.json", "r") as read_file:
+        JsDates = json.load(read_file)
+    sql = '''INSERT INTO dates(date, event) VALUES(?, ?)'''
+    for i in JsDates.keys():
+        data_tuple = (i, JsDates[i])
+        cur.execute(sql, data_tuple)
+        conn.commit()
+
+def usersDB_rewrite():
+    # Этот файл нужен для переписывания базы данных пользователей в случае нужды
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users( 
+       id INT PRIMARY KEY,
+       username TEXT,
+       name TEXT,
+       regs DATETIME)
+    """)
+
+def datesJS_rewrite():
+    with open('dates.txt', encoding='utf-8') as file:
+        info = file.readlines()
+        # print(info)
+    jsn = {}
+    for i in info:
+        i = ' ' + i.strip()
+        # print(i)
+        if i[1] != '#':
+            # lol += 1
+            # print(i.split(' – ')[1][-1], lol)
+            jsn[i.split(' – ')[0]] = i.split(' – ')[1]
+        # else:
+        # jsn.append(i)
+    # print(*txtDates)
+    print(jsn)
+    with open("dates.json", "w") as file:
+        json.dump(jsn, file)
+    '''with open("dates.json", "r") as read_file:
+        JsDates = json.load(read_file)
+    print(JsDates)'''
+
+
+def datesTXTmoderniser():
+    with open('dates.txt', encoding='utf-8') as file:
+        info = file.readlines()
+        info = list(map(lambda x: ' ' + x, info))
+    with open('dates.txt', encoding='utf-8', mode='w') as file:
+        file.write(''.join(info))
+
+
+def unpacker(listt):
+    spis = []
+    for i in listt:
+        spis.append(f'{i[0]} - {i[1]}')
+    return spis
+
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram.utils import executor
 import sqlite3
 import datetime
 import json
+from helper import unpacker
 
 # Создание бота
 TOKEN = '5165988091:AAGZ1qrF6r7f8cB_crlOzX0dpHltfViCyA8'
@@ -12,6 +13,8 @@ dp = Dispatcher(bot)
 # Создание соединений с файлами, вероятно перемещение их в отдельный файл
 conn = sqlite3.connect('users.db')
 cur = conn.cursor()
+connection = sqlite3.connect('dates.db')
+cursor = connection.cursor()
 with open('dates.txt', 'r', encoding='utf-8') as file:
     txtDates = file.readlines()
 with open("dates.json", "r") as read_file:
@@ -42,7 +45,7 @@ async def view(msg: types.Message):
     '''with open('dates.txt', 'r', encoding='utf-8') as file:
         txtDates = file.readlines()'''
     for x in range(0, len('\n'.join(txtDates)), 4096):
-        await bot.send_message(msg.chat.id, '\n'.join(txtDates)[x:x + 4096])
+        await bot.send_message(msg.chat.id, '\n'.join(txtDates)[x:x + 4096], parse_mode='markdown')
 
 
 @dp.message_handler(commands=['browse'])  # Первый прототип поиска по датам
@@ -50,7 +53,28 @@ async def search(msg: types.Message):
     '''with open("dates.json", "r") as read_file:
         JsDates = json.load(read_file)'''
     argument = msg.get_args()  # Получение даты
-    # TODO Создать механизм поиска нужной даты!
+    # print(JsDates.keys())
+    try:
+        if argument.isdigit():
+            if len(argument) == 3:  # TODO Доделать отсеивание лишних дат. ПРИМЕР: arg: 1945 , 1945 in ans
+                date = cursor.execute(f''' SELECT * FROM dates WHERE date like '%{argument}%' ''').fetchall()
+                if date:
+                # print(date)
+                    await bot.send_message(msg.chat.id, '\n'.join(unpacker(date)))
+                else:
+                    await msg.reply('Ничего не найдено!')
+            elif len(argument) >= 4:
+                date = cursor.execute(f''' SELECT * FROM dates WHERE date like '%{argument}%' ''').fetchall()
+                if date:
+                    await bot.send_message(msg.chat.id, '\n'.join(unpacker(date)))
+                else:
+                    await msg.reply('Ничего не найдено!')
+            else:
+                await msg.reply('Извините, я не могу обработать запрос короче 3-х символов.')
+        else:
+            await msg.reply('Пожалуйста, используйте цифры.')
+    except:
+        await msg.reply('Ошибка запроса! Попробуйте вписать запрос по шаблону!')
 
 
 if __name__ == '__main__':
